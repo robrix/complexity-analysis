7ae0a53afb9bf1763cdd7d96f247f841df484f01
Rob Rix <rob.rix@me.com>
2017-10-12 10:40:05 -0400

    Define paramorphisms.

--- src/Data/Complexity.hs
+++ src/Data/Complexity.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DeriveFoldable, DeriveFunctor #-}
 module Data.Complexity where
 
+import Control.Arrow ((&&&))
+
 newtype Name = Name String
   deriving (Eq, Ord, Read, Show)
 
@@ -26,6 +28,10 @@ newtype Term f = In { out :: f (Term f) }
 data Attr f a = Attr { attribute :: a, hole :: f (Attr f a) }
 
 type FAlgebra f a = f a -> a
+type RAlgebra f a = f (Term f, a) -> a
 
 cata :: Functor f => FAlgebra f a -> Term f -> a
 cata algebra = go where go = algebra . fmap go . out
+
+para :: Functor f => RAlgebra f a -> Term f -> a
+para algebra = go where go = algebra . fmap (id &&& go) . out
