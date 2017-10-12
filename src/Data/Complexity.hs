d80a05d20df02fb495fca643c431b578c8cf8de2
Rob Rix <rob.rix@me.com>
2017-10-12 10:50:20 -0400

    :fire: most of the Complexity constructors.

--- src/Data/Complexity.hs
+++ src/Data/Complexity.hs
@@ -9,14 +9,7 @@ newtype Name = Name String
 
 data Complexity i
   = Constant i
-  | ForAll Name (Complexity i)
-  | CVar Name
-  | Complexity i :* Complexity i
-  | Complexity i :-> Complexity i
   deriving (Eq, Ord, Read, Show)
-
-infixl 7 :*
-infixr 9 :->
 
 
 data Expr a
