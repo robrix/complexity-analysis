dcce1bb80950e059f01d347da7411c410d03d2d0
Rob Rix <rob.rix@me.com>
2017-10-13 00:37:58 -0400

    Use the types from free & recursion-schemes.

--- src/Data/Complexity.hs
+++ src/Data/Complexity.hs
@@ -1,13 +1,18 @@
 {-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable, FlexibleInstances, FunctionalDependencies, GeneralizedNewtypeDeriving, MultiParamTypeClasses, StandaloneDeriving, TypeFamilies, UndecidableInstances #-}
 module Data.Complexity where
 
+import Control.Comonad (extract)
+import Control.Comonad.Cofree
+import qualified Control.Comonad.Trans.Cofree as F
+import Control.Monad.Free
+import qualified Control.Monad.Trans.Free as F
 import Control.Monad.Fresh
 import Control.Monad.Reader
 import Control.Monad.State
 import Data.Bifunctor (second)
 import Data.Foldable (fold)
 import Data.Function (on)
-import Data.Functor.Foldable (Recursive(..), Base)
+import Data.Functor.Foldable (Recursive(..), Fix(..))
 import qualified Data.List as List
 import Data.Maybe (fromMaybe)
 import Data.Semigroup (Semigroup(..))
@@ -32,41 +37,41 @@ data Expr a
   | Snd a
   deriving (Eq, Foldable, Functor, Ord, Read, Show, Traversable)
 
-makeAbs :: Name -> Term Expr -> Term Expr
-makeAbs name body = In (Abs name body)
+makeAbs :: Name -> Fix Expr -> Fix Expr
+makeAbs name body = Fix (Abs name body)
 
-lam :: (Term Expr -> Term Expr) -> Term Expr
+lam :: (Fix Expr -> Fix Expr) -> Fix Expr
 lam hoas = makeAbs n body
   where n = maybe (Name 0) succ (maxBoundVariable body)
         body = hoas (var n)
 
-maxBoundVariable :: Term Expr -> Maybe Name
+maxBoundVariable :: Fix Expr -> Maybe Name
 maxBoundVariable = cata $ \ expr -> case expr of
   Abs name _ -> Just name
   _          -> foldr max Nothing expr
 
-(#) :: Term Expr -> Term Expr -> Term Expr
-func # arg = In (App func arg)
+(#) :: Fix Expr -> Fix Expr -> Fix Expr
+func # arg = Fix (App func arg)
 
 infixl 9 #
 
-var :: Name -> Term Expr
-var name = In (Var name)
+var :: Name -> Fix Expr
+var name = Fix (Var name)
 
-bool :: Bool -> Term Expr
-bool b = In (Lit b)
+bool :: Bool -> Fix Expr
+bool b = Fix (Lit b)
 
-iff :: Term Expr -> Term Expr -> Term Expr -> Term Expr
-iff c t e = In (If c t e)
+iff :: Fix Expr -> Fix Expr -> Fix Expr -> Fix Expr
+iff c t e = Fix (If c t e)
 
-pair :: Term Expr -> Term Expr -> Term Expr
-pair fst snd = In (Pair fst snd)
+pair :: Fix Expr -> Fix Expr -> Fix Expr
+pair fst snd = Fix (Pair fst snd)
 
-pfst :: Term Expr -> Term Expr
-pfst pair = In (Fst pair)
+pfst :: Fix Expr -> Fix Expr
+pfst pair = Fix (Fst pair)
 
-psnd :: Term Expr -> Term Expr
-psnd pair = In (Snd pair)
+psnd :: Fix Expr -> Fix Expr
+psnd pair = Fix (Snd pair)
 
 
 data Type a
@@ -80,72 +85,26 @@ data Type a
 infixr 0 :->
 infixl 7 :*
 
-freeTypeVariables :: CoAttr Type a -> Set.Set Name
+freeTypeVariables :: Free Type a -> Set.Set Name
 freeTypeVariables = cata $ \ ty -> case ty of
-  ContinueF (TVar name)        -> Set.singleton name
-  ContinueF (ForAll name body) -> Set.delete name body
-  _                            -> fold ty
+  F.Free (TVar name)        -> Set.singleton name
+  F.Free (ForAll name body) -> Set.delete name body
+  _                         -> fold ty
 
-returnType :: CoAttr Type a -> Maybe (CoAttr Type a)
-returnType (Continue (_ :-> returnTy)) = Just returnTy
-returnType (Stop err)                  = Just (Stop err)
-returnType _                           = Nothing
+returnType :: Free Type a -> Maybe (Free Type a)
+returnType (Free (_ :-> returnTy)) = Just returnTy
+returnType (Pure err)              = Just (Pure err)
+returnType _                       = Nothing
 
-fstType :: CoAttr Type a -> Maybe (CoAttr Type a)
-fstType (Continue (fstTy :* _)) = Just fstTy
-fstType (Stop err)              = Just (Stop err)
-fstType _                       = Nothing
+fstType :: Free Type a -> Maybe (Free Type a)
+fstType (Free (fstTy :* _)) = Just fstTy
+fstType (Pure err)          = Just (Pure err)
+fstType _                   = Nothing
 
-sndType :: CoAttr Type a -> Maybe (CoAttr Type a)
-sndType (Continue (_ :* sndTy)) = Just sndTy
-sndType (Stop err)              = Just (Stop err)
-sndType _                       = Nothing
-
-
-newtype Term f = In { out :: f (Term f) }
-
-type instance Base (Term f) = f
-
-instance Functor f => Recursive (Term f) where project = out
-
-deriving instance Eq (f (Term f)) => Eq (Term f)
-deriving instance Ord (f (Term f)) => Ord (Term f)
-deriving instance Read (f (Term f)) => Read (Term f)
-deriving instance Show (f (Term f)) => Show (Term f)
-
-data Attr f a = Attr { attr :: a, hole :: f (Attr f a) }
-
-data AttrF f a b = AttrF { attrF :: a, holeF :: f b }
-  deriving (Eq, Foldable, Functor, Ord, Read, Show, Traversable)
-
-type instance Base (Attr f a) = AttrF f a
-
-instance Functor f => Recursive (Attr f a) where project (Attr a f) = AttrF a f
-
-deriving instance (Eq (f (Attr f a)), Eq a) => Eq (Attr f a)
-deriving instance (Ord (f (Attr f a)), Ord a) => Ord (Attr f a)
-deriving instance (Read (f (Attr f a)), Read a) => Read (Attr f a)
-deriving instance (Show (f (Attr f a)), Show a) => Show (Attr f a)
-
-data CoAttr f a
-  = Stop a
-  | Continue (f (CoAttr f a))
-
-data CoAttrF f a b
-  = StopF a
-  | ContinueF (f b)
-  deriving (Eq, Foldable, Functor, Ord, Read, Show, Traversable)
-
-type instance Base (CoAttr f a) = CoAttrF f a
-
-instance Functor f => Recursive (CoAttr f a) where
-  project (Stop a)     = StopF a
-  project (Continue f) = ContinueF f
-
-deriving instance (Eq (f (CoAttr f a)), Eq a) => Eq (CoAttr f a)
-deriving instance (Ord (f (CoAttr f a)), Ord a) => Ord (CoAttr f a)
-deriving instance (Read (f (CoAttr f a)), Read a) => Read (CoAttr f a)
-deriving instance (Show (f (CoAttr f a)), Show a) => Show (CoAttr f a)
+sndType :: Free Type a -> Maybe (Free Type a)
+sndType (Free (_ :* sndTy)) = Just sndTy
+sndType (Pure err)          = Just (Pure err)
+sndType _                   = Nothing
 
 
 newtype Env a = Env { getEnv :: [(Name, a)] }
@@ -183,80 +142,80 @@ substExtend name value = (substSingleton name value <>)
 class Binder value where
   substitute :: Subst value -> value -> value
 
-instance Binder (CoAttr Type Error) where
+instance Binder (Free Type Error) where
   substitute = flip (cata (\ ty subst -> case ty of
-    ContinueF (TVar name)        -> fromMaybe (Continue (TVar name)) (substLookup name subst)
-    ContinueF (ForAll name body) -> Continue (ForAll name (body (substDelete name subst)))
-    ContinueF other              -> Continue (($ subst) <$> other)
-    StopF err                    -> Stop err))
+    F.Free (TVar name)        -> fromMaybe (Free (TVar name)) (substLookup name subst)
+    F.Free (ForAll name body) -> Free (ForAll name (body (substDelete name subst)))
+    F.Free other              -> Free (($ subst) <$> other)
+    F.Pure err                -> Pure err))
 
 
 data Error
   = FreeVariable Name
-  | TypeMismatch (Type (CoAttr Type Error)) (Type (CoAttr Type Error))
-  | InfiniteType Name (CoAttr Type Error)
+  | TypeMismatch (Type (Free Type Error)) (Type (Free Type Error))
+  | FixfiniteType Name (Free Type Error)
   deriving (Eq, Ord, Read, Show)
 
-type Elab = StateT (Subst (CoAttr Type Error)) (ReaderT (Env (Term Type)) (Fresh Name))
+type Elab = StateT (Subst (Free Type Error)) (ReaderT (Env (Fix Type)) (Fresh Name))
 
-runElab :: Elab a -> (a, Subst (CoAttr Type Error))
+runElab :: Elab a -> (a, Subst (Free Type Error))
 runElab = fst . flip runFresh (Name 0) . flip runReaderT mempty . flip runStateT mempty
 
-substElaborated :: Attr Expr (CoAttr Type Error) -> Subst (CoAttr Type Error) -> Attr Expr (CoAttr Type Error)
-substElaborated = cata (\ (AttrF ty expr) subst -> Attr (substitute subst ty) (($ subst) <$> expr))
+substElaborated :: Cofree Expr (Free Type Error) -> Subst (Free Type Error) -> Cofree Expr (Free Type Error)
+substElaborated = cata (\ (ty F.:< expr) subst -> substitute subst ty :< (($ subst) <$> expr))
 
 
-elaborate :: Term Expr -> Elab (Attr Expr (CoAttr Type Error))
-elaborate (In (Abs n b)) = do
+elaborate :: Fix Expr -> Elab (Cofree Expr (Free Type Error))
+elaborate (Fix (Abs n b)) = do
   t <- fresh
-  b' <- local (envExtend n (In (TVar t))) (elaborate b)
-  pure (Attr (Continue (Continue (TVar t) :-> attr b')) (Abs n b'))
-elaborate (In (App f a)) = do
+  b' <- local (envExtend n (Fix (TVar t))) (elaborate b)
+  pure (Free (Free (TVar t) :-> extract b') :< Abs n b')
+elaborate (Fix (App f a)) = do
   t <- fresh
   f' <- elaborate f
   a' <- elaborate a
-  fTy <- unify (attr f') (Continue (attr a' :-> Continue (TVar t)))
-  pure (Attr (fromMaybe (Continue (TVar t)) (returnType fTy)) (App f' a'))
-elaborate (In (Var name)) = do
+  fTy <- unify (extract f') (Free (extract a' :-> Free (TVar t)))
+  pure (fromMaybe (Free (TVar t)) (returnType fTy) :< App f' a')
+elaborate (Fix (Var name)) = do
   env <- ask
-  pure (Attr (maybe (Stop (FreeVariable name)) (cata Continue) (envLookup name env)) (Var name))
-elaborate (In (Lit b)) = pure (Attr (Continue Bool) (Lit b))
-elaborate (In (If c t e)) = do
+  pure (maybe (Pure (FreeVariable name)) (cata Free) (envLookup name env) :< Var name)
+elaborate (Fix (Lit b)) = pure (Free Bool :< Lit b)
+elaborate (Fix (If c t e)) = do
   c' <- elaborate c
   t' <- elaborate t
   e' <- elaborate e
-  result <- unify (attr t') (attr e')
-  pure (Attr result (If c' t' e'))
-elaborate (In (Pair fst snd)) = do
+  result <- unify (extract t') (extract e')
+  pure (result :< If c' t' e')
+elaborate (Fix (Pair fst snd)) = do
   fst' <- elaborate fst
   snd' <- elaborate snd
-  pure (Attr (Continue (attr fst' :* attr snd')) (Pair fst' snd'))
-elaborate (In (Fst pair)) = do
+  pure (Free (extract fst' :* extract snd') :< Pair fst' snd')
+elaborate (Fix (Fst pair)) = do
   t1 <- fresh
   t2 <- fresh
   pair' <- elaborate pair
-  pairTy <- unify (attr pair') (Continue (Continue (TVar t1) :* Continue (TVar t2)))
-  pure (Attr (fromMaybe (Continue (TVar t1)) (fstType pairTy)) (Fst pair'))
-elaborate (In (Snd pair)) = do
+  pairTy <- unify (extract pair') (Free (Free (TVar t1) :* Free (TVar t2)))
+  pure (fromMaybe (Free (TVar t1)) (fstType pairTy) :< Fst pair')
+elaborate (Fix (Snd pair)) = do
   t1 <- fresh
   t2 <- fresh
   pair' <- elaborate pair
-  pairTy <- unify (attr pair') (Continue (Continue (TVar t1) :* Continue (TVar t2)))
-  pure (Attr (fromMaybe (Continue (TVar t2)) (sndType pairTy)) (Fst pair'))
+  pairTy <- unify (extract pair') (Free (Free (TVar t1) :* Free (TVar t2)))
+  pure (fromMaybe (Free (TVar t2)) (sndType pairTy) :< Snd pair')
 
 
-unify :: CoAttr Type Error -> CoAttr Type Error -> Elab (CoAttr Type Error)
-unify (Stop err1)   _             = pure (Stop err1)
-unify _             (Stop err2)   = pure (Stop err2)
-unify (Continue t1) (Continue t2)
-  | t1 == t2  = pure (Continue t2)
+unify :: Free Type Error -> Free Type Error -> Elab (Free Type Error)
+unify (Pure err1)   _             = pure (Pure err1)
+unify _             (Pure err2)   = pure (Pure err2)
+unify (Free t1) (Free t2)
+  | t1 == t2  = pure (Free t2)
   | otherwise = case (t1, t2) of
-    (a1 :-> b1,  a2 :-> b2)  -> (Continue .) . (:->) <$> unify a1 a2 <*> unify b1 b2
-    (TVar name1, _)          -> bind name1 (Continue t2)
-    (_,          TVar name2) -> bind name2 (Continue t1)
-    (t1,         t2)         -> pure (Stop (TypeMismatch t1 t2))
+    (a1 :-> b1,  a2 :-> b2)  -> (Free .) . (:->) <$> unify a1 a2 <*> unify b1 b2
+    (TVar name1, _)          -> bind name1 (Free t2)
+    (_,          TVar name2) -> bind name2 (Free t1)
+    (t1,         t2)         -> pure (Pure (TypeMismatch t1 t2))
 
-bind :: Name -> CoAttr Type Error -> Elab (CoAttr Type Error)
+bind :: Name -> Free Type Error -> Elab (Free Type Error)
 bind name ty
-  | Set.member name (freeTypeVariables ty) = pure (Stop (InfiniteType name ty))
+  | Set.member name (freeTypeVariables ty) = pure (Pure (FixfiniteType name ty))
   | otherwise                              = modify (substExtend name ty) >> pure ty
