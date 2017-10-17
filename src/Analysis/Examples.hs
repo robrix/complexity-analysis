8173a9b2698e89b855dba00209e04f6b2fa78afe
Rob Rix <rob.rix@me.com>
2017-10-17 16:17:26 -0400

    Define a foldr term.

--- src/Analysis/Examples.hs
+++ src/Analysis/Examples.hs
@@ -1,1 +1,6 @@
 module Analysis.Examples where
+
+import Data.Expr as Expr
+
+foldr :: Term
+foldr = rec (\ foldr -> lam (\ combine -> lam (\ seed -> lam (\ list -> unlist seed (lam (\ a -> lam (\ as -> combine # a # (foldr # combine # seed # as)))) list))))
