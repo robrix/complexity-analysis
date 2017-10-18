8fabadf6a94cf8e9080d61ef4880e40340468c62
Rob Rix <rob.rix@me.com>
2017-10-18 14:56:47 -0400

    Define Eq & Ord instances for Rec.

--- src/Data/Rec.hs
+++ src/Data/Rec.hs
@@ -7,5 +7,11 @@ newtype Rec expr a = Rec (expr a (Rec expr a))
 unRec :: Rec expr a -> expr a (Rec expr a)
 unRec (Rec expr) = expr
 
+instance (Eq1 (expr a), Eq a) => Eq (Rec expr a) where
+  Rec expr1 == Rec expr2 = liftEq (==) expr1 expr2
+
+instance (Ord1 (expr a), Ord a) => Ord (Rec expr a) where
+  compare (Rec expr1) (Rec expr2) = liftCompare compare expr1 expr2
+
 instance (Show1 (expr a), Show a) => Show (Rec expr a) where
   showsPrec d (Rec expr) = showsUnaryWith (liftShowsPrec showsPrec showList) "Rec" d expr
