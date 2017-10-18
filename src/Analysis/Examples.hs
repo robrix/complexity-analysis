a9fdbea2664b42cb76eed0b2d88f33b887c05131
Rob Rix <rob.rix@me.com>
2017-10-18 16:35:15 -0400

    foldrT and mapT are Total.

--- src/Analysis/Examples.hs
+++ src/Analysis/Examples.hs
@@ -1,13 +1,12 @@
 module Analysis.Examples where
 
 import Data.Expr as Expr
-import Data.Rec
 import Data.Type as Type
 
 foldr :: Term Expr
 foldr = letRec (\ foldr -> lam (\ combine -> lam (\ seed -> lam (\ list -> unlist seed (lam (\ a -> lam (\ as -> combine # a # (foldr # combine # seed # as)))) list))))
 
-foldrT :: Rec (Partial Type) Error
+foldrT :: Total Type
 foldrT = forAllT (\ each -> forAllT (\ result -> (each .-> result .-> result) .-> result .-> listT each .-> result))
 
 -- $
@@ -18,7 +17,7 @@ foldrT = forAllT (\ each -> forAllT (\ result -> (each .-> result .-> result) .-
 map :: Term Expr
 map = letRec (\ map -> lam (\ f -> lam (\ list -> unlist nil (lam (\ a -> lam (\ as -> cons (f # a) (map # f # as)))) list)))
 
-mapT :: Rec (Partial Type) Error
+mapT :: Total Type
 mapT = forAllT (\ element -> forAllT (\ mapped -> (element .-> mapped) .-> listT element .-> listT mapped))
 
 -- $
