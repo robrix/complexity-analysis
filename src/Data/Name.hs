7f5df58b6decc4650f90de3df5ed7964ade94528
Rob Rix <rob.rix@me.com>
2017-10-18 15:35:08 -0400

    Pretty-print Name.

--- src/Data/Name.hs
+++ src/Data/Name.hs
@@ -2,6 +2,7 @@
 module Data.Name
 ( Name(..)
 , unName
+, prettyName
 ) where
 
 newtype Name = Name Int
@@ -9,3 +10,16 @@ newtype Name = Name Int
 
 unName :: Name -> Int
 unName (Name n) = n
+
+
+prettyName :: Int -> Name -> ShowS
+prettyName _ = alpha . unName
+  where alpha n | n < 0     = showString ""
+                | otherwise = let (next, place) = (n `divMod` alphabetLength) in
+                  alpha (pred next) . showChar (alphabet !! place)
+
+alphabet :: String
+alphabet = ['a'..'z']
+
+alphabetLength :: Int
+alphabetLength = length alphabet
