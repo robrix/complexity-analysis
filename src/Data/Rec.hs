8d71873288bb8b568f885cd730e99a6c45aff16a
Rob Rix <rob.rix@me.com>
2017-10-18 14:55:36 -0400

    Define a Show instance for Rec.

--- src/Data/Rec.hs
+++ src/Data/Rec.hs
@@ -1,6 +1,11 @@
 module Data.Rec where
 
+import Data.Functor.Classes
+
 newtype Rec expr a = Rec (expr a (Rec expr a))
 
 unRec :: Rec expr a -> expr a (Rec expr a)
 unRec (Rec expr) = expr
+
+instance (Show1 (expr a), Show a) => Show (Rec expr a) where
+  showsPrec d (Rec expr) = showsUnaryWith (liftShowsPrec showsPrec showList) "Rec" d expr
