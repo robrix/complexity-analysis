e2ecdb86458708e0d71cd5704bdfdae627239f73
Rob Rix <rob.rix@me.com>
2017-10-17 21:26:31 -0400

    Generalize the type of foldr.

--- src/Analysis/Examples.hs
+++ src/Analysis/Examples.hs
@@ -1,11 +1,10 @@
 module Analysis.Examples where
 
 import Data.Expr as Expr
-import Data.Name
 import Data.Type as Type
 
 foldr :: Term
 foldr = rec (\ foldr -> lam (\ combine -> lam (\ seed -> lam (\ list -> unlist seed (lam (\ a -> lam (\ as -> combine # a # (foldr # combine # seed # as)))) list))))
 
 foldrT :: PartialType
-foldrT = (tvar (Name 0) .-> tvar (Name 1) .-> tvar (Name 1)) .-> tvar (Name 1) .-> listT (tvar (Name 0)) .-> tvar (Name 1)
+foldrT = forAllT (\ each -> forAllT (\ result -> (each .-> result .-> result) .-> result .-> listT each .-> result))
