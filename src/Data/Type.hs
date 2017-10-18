99a69d40075ec2ae299acd33d6797ef2aace7936
Rob Rix <rob.rix@me.com>
2017-10-18 12:33:42 -0400

    Derive a Read instance for Partial.

--- src/Data/Type.hs
+++ src/Data/Type.hs
@@ -41,6 +41,7 @@ type TotalType = Fix Type
 data Partial expr error = Continue (expr (Partial expr error)) | Stop error
 deriving instance (Eq   (f (Partial f a)), Eq   a) => Eq   (Partial f a)
 deriving instance (Ord  (f (Partial f a)), Ord  a) => Ord  (Partial f a)
+deriving instance (Read (f (Partial f a)), Read a) => Read (Partial f a)
 deriving instance (Show (f (Partial f a)), Show a) => Show (Partial f a)
 
 data PartialF expr error recur = ContinueF (expr recur) | StopF error
