c96cc62a88abfe8e7292d4394abc5882e3001066
Rob Rix <rob.rix@me.com>
2017-10-19 10:47:37 -0400

    Define a Pointwise Semigroup, something like ZipList.

--- src/Data/Module.hs
+++ src/Data/Module.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses #-}
 module Data.Module where
 
 import Data.Semigroup
@@ -12,3 +12,12 @@ class (Monoid m, Semigroup m, Semiring r) => Module r m where
 instance Semiring r => Module r [r] where
   _ ><< []     = []
   r ><< (x:xs) = r >< x : r ><< xs
+
+
+newtype Pointwise a = Pointwise { unPointwise :: [a] }
+  deriving (Eq, Ord, Show)
+
+instance Semigroup a => Semigroup (Pointwise a) where
+  Pointwise (a : as) <> Pointwise (b : bs) = Pointwise (a <> b : as <> bs)
+  Pointwise [] <> Pointwise bs = Pointwise bs
+  Pointwise as <> Pointwise [] = Pointwise as
