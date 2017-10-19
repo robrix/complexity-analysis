44188380ea2b7d22a82f8a6db4b56b84c9d166af
Rob Rix <rob.rix@me.com>
2017-10-19 11:41:23 -0400

    :memo: (><<).

--- src/Data/Module.hs
+++ src/Data/Module.hs
@@ -8,6 +8,35 @@ import Data.These
 
 -- | A (left) module over some 'Semiring' @r@.
 class (Monoid m, Semigroup m, Semiring r) => Module r m where
+  -- | Multiply a module by a scalar (on the left).
+  --
+  --   Laws:
+  --
+  --   The 'Semigroup' laws (associativity of '(<>)' & equality of '(<>)' and 'mappend'):
+  --
+  --   > x <> (y <> z) = (x <> y) <> z
+  --   > (<>) = mappend
+  --
+  --   The 'Monoid' laws ('zero' is the left- and right-identity of '(<>)'):
+  --
+  --   > zero <> x = x
+  --   > x <> zero = x
+  --
+  --   'one' is the left-identity of '(><<)':
+  --
+  --   > one ><< x = x
+  --
+  --   '(><<)' is left-distributive over 'Module' '(<>)':
+  --
+  --   > r ><< (x <> y) = (r ><< x) <> (r ><< y)
+  --
+  --   '(><<)' is right-distributive over 'Semiring' '(<>)':
+  --
+  --   > (r <> s) ><< x = (r ><< x) <> (s ><< x)
+  --
+  --   '(><<)' is right-distributive over 'Semiring' '(><)':
+  --
+  --   > r >< s ><< x = r ><< (s ><< x)
   (><<) :: r -> m -> m
 
   infixl 7 ><<
