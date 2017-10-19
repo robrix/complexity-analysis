7318809f26889604fca284e8dacfbcddf75ca287
Rob Rix <rob.rix@me.com>
2017-10-19 10:56:48 -0400

    Define a Module instance for Pointwise.

--- src/Data/Module.hs
+++ src/Data/Module.hs
@@ -26,6 +26,9 @@ instance Monoid a => Monoid (Pointwise a) where
   mempty = Pointwise []
   mappend = alignWith (mergeThese mappend)
 
+instance Semiring r => Module r (Pointwise r) where
+  (><<) = fmap . (><)
+
 instance Align Pointwise where
   nil = Pointwise []
   alignWith f (Pointwise as) (Pointwise bs) = Pointwise (alignWith f as bs)
