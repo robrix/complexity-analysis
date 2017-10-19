63962e0d5e2fd2a24a68856ec5c98af05e9d3543
Rob Rix <rob.rix@me.com>
2017-10-18 20:17:32 -0400

    Define a FreeTypeVariables1 class.

--- src/Data/FreeTypeVariables.hs
+++ src/Data/FreeTypeVariables.hs
@@ -7,5 +7,9 @@ import qualified Data.Set as Set
 class FreeTypeVariables t where
   freeTypeVariables :: t -> Set.Set Name
 
+class FreeTypeVariables1 t where
+  liftFreeTypeVariables :: (a -> Set.Set Name) -> t a -> Set.Set Name
+
+
 instance FreeTypeVariables (Set.Set Name) where
   freeTypeVariables = id
