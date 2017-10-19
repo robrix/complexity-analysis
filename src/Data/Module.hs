b54930b0b7798a6eda44ef85a5dbc42071a51e03
Rob Rix <rob.rix@me.com>
2017-10-19 10:44:20 -0400

    Define a Module instance for lists of values in a Semiring.

--- src/Data/Module.hs
+++ src/Data/Module.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
 module Data.Module where
 
 import Data.Semigroup
@@ -8,3 +8,7 @@ class (Monoid m, Semigroup m, Semiring r) => Module r m where
   (><<) :: r -> m -> m
 
   infixl 7 ><<
+
+instance Semiring r => Module r [r] where
+  _ ><< []     = []
+  r ><< (x:xs) = r >< x : r ><< xs
