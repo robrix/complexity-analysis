c41bd122798daa1247d456d81ff95834841dd2bd
Rob Rix <rob.rix@me.com>
2017-10-19 17:16:46 -0400

    Derive a Generic instance.

--- src/Data/Cost.hs
+++ src/Data/Cost.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Data.Cost where
 
 import Data.Name
+import GHC.Generics
 
 data Cost
   = Const Int
   | CVar Name
   | Plus Cost Cost
   | Times Cost Cost
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Generic, Ord, Show)
